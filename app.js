9fa4109054877f821e9b49eb856ddd8a8597c8c3
tbricca <tbricca@gmail.com>
2018-09-14 07:37:17 -0700

    adding in logic to be able to add routes

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 /// DEFINE UI VARIABLES
 const form = document.querySelector("#route-form");
+const routeInput = document.querySelector("#route");
+
+// Load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+  form.addEventListener("submit", addRoute);
+}
+
+function addRoute(e) {
+  if (routeInput.value === "") {
+    alert("Add a Route");
+  }
+}
